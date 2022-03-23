01f7fcc818b91daf6de4b9825c2ab90379e2593e
Easy <crazybeginner@qq.com>
2022-03-23 13:19:16 +0800

    修改了端口号

--- smqtt-ui/src/services/api.js
+++ smqtt-ui/src/services/api.js
@@ -1,14 +1,14 @@
 //跨域代理前缀
-const API_PROXY_PREFIX='/api'
+const API_PROXY_PREFIX = '/api'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
 const IP = window.location.host.split(':')[0]
 module.exports = {
-  LOGIN: `http://${IP}:18887/auth/login`,
-  CLUSTERS: `http://${IP}:18887/smqtt/cluster`,
-  CONNECTIONS: `http://${IP}:18887/smqtt/connection`,
-  SUBSCRIBES:`http://${IP}:18887/smqtt/subscribe`,
-  ISCLUESTER:`http://${IP}:18887/smqtt/is/cluster`,
-  PUBLISH:`http://${IP}:18887/smqtt/publish`,
-  ROUTES: `${BASE_URL}/routes`,
-  ACLACTION: `http://${IP}:60000/smqtt/acl/`
+    LOGIN: `http://${IP}:18887/auth/login`,
+    CLUSTERS: `http://${IP}:18887/smqtt/cluster`,
+    CONNECTIONS: `http://${IP}:18887/smqtt/connection`,
+    SUBSCRIBES: `http://${IP}:18887/smqtt/subscribe`,
+    ISCLUESTER: `http://${IP}:18887/smqtt/is/cluster`,
+    PUBLISH: `http://${IP}:18887/smqtt/publish`,
+    ROUTES: `${BASE_URL}/routes`,
+    ACLACTION: `http://${IP}:18887/smqtt/acl/`
 }
